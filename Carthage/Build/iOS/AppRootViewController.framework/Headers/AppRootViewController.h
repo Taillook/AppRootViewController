4c7c1f5d2a26b1ff5634863289b3bc922ddcb957
Taillook <jousoku@gmail.com>
2018-03-27 15:47:09 +0900

    cart

---
+++ Carthage/Build/iOS/AppRootViewController.framework/Headers/AppRootViewController.h
@@ -0,0 +1,19 @@
+//
+//  AppRootViewController.h
+//  AppRootViewController
+//
+//  Created by Taillook on 2018/03/27.
+//  Copyright © 2018年 Taillook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AppRootViewController.
+FOUNDATION_EXPORT double AppRootViewControllerVersionNumber;
+
+//! Project version string for AppRootViewController.
+FOUNDATION_EXPORT const unsigned char AppRootViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppRootViewController/PublicHeader.h>
+
+
